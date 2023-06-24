94915016ab4d2d46221d417635bb2b967e92262a
WBT <WBT@users.noreply.github.com>
2023-06-24 16:58:19 -0400

    Add logo & favicons

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -87,7 +87,7 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="WiserRiser logo" className="cursor-pointer" fill src="/logo1padded.png" style={{borderRadius: "15%" }} />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">WiserRiser</span>

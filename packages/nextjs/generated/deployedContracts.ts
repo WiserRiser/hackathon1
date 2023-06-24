6dd7b035a7d76ea826dfb93e056887be3d13a401
tiffany6.eth <tiffanyy.dev@gmail.com>
2023-06-24 18:28:23 -0400

    Revert "Merge branch 'main' into gnosis-safe-integration-for-aa"
    This reverts commit 9e1112ca988bf585e0f477a114efd2fdb55190ff, reversing
    changes made to a9da4062c588b4cfad565d9f5b37c5dada9a81bb.

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -8,7 +8,7 @@ const contracts = {
           address: "0x7D1916883d9f6D42Ff0ecAeB9ce90CC08295fD2A",
         },
         CommunityToken: {
-          address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+          address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
           abi: [
             {
               inputs: [],
--- packages/nextjs/lib/consts.ts
+++
@@ -1,2 +0,0 @@
-export const APP_NAME = "My World ID App";
-export const POLYGON_SERVER_URI = "";

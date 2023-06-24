bf89bf2c4ecc7879862ca6846869bd0107a340da
tiffany6.eth <tiffanyy.dev@gmail.com>
2023-06-24 17:05:05 -0400

    added the gelator relayer for sponsored transaction

---
+++ packages/hardhat/contracts/SimpleCounter.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.8.17;
+
+contract SimpleCounter {
+    uint256 public counter;
+
+    event IncrementCounter(uint256 newCounterValue, address msgSender);
+
+    // `increment` is the target function to call.
+    // This function increments a counter variable by 1
+    // IMPORTANT: with `sponsoredCall` you need to implement
+    // your own smart contract security measures, as this
+    // function can be called by any third party and not only by
+    // Gelato Relay. If not done properly, funds kept in this
+    // smart contract can be stolen.
+    function increment() external {
+        counter++;
+        emit IncrementCounter(counter, msg.sender);
+    }
+}

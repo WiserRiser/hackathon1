b85e17cb8eaf2541e160712d3ed19f0a8bd40f1e
WBT <WBT@users.noreply.github.com>
2023-06-25 02:03:31 -0400

    Drop ERC721URIStorage

--- packages/hardhat/contracts/PostToken.sol
+++ packages/hardhat/contracts/PostToken.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PostToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract PostToken is ERC721, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
@@ -65,21 +64,11 @@ contract PostToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
     }
 
     // The following function is an override required by Solidity for ERC721URIStorage.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721) {
         super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 }

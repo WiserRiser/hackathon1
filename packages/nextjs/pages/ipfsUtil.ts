c9ba2ab3df90e81a213b91953c1a8be1c7ef9dee
WBT <WBT@users.noreply.github.com>
2023-06-24 22:48:13 -0400

    Change env var names

--- packages/nextjs/pages/ipfsUtil.ts
+++ packages/nextjs/pages/ipfsUtil.ts
@@ -2,7 +2,7 @@ import { create } from 'ipfs-http-client';
 
 export const storeInIPFS = async (data: string): Promise<string> => {
   const auth =
-    'Basic ' + Buffer.from(process.env.INFURA_ID + ':' + process.env.INFURA_SECRET_KEY).toString('base64');
+    'Basic ' + Buffer.from(process.env.NEXT_PUBLIC_INFURA_ID + ':' + process.env.NEXT_PUBLIC_INFURA_SECRET_KEY).toString('base64');
   const ipfs = create({
     host: 'ipfs.infura.io',
     port: 5001,
